8def063b0148957c993d1b4732838ff1d21fca45
Athena Serene <aserenehoosang@gmail.com>
2022-11-05 13:08:13 -0700

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 const PORT = 3003
 app.get('/greeting/:name', (request, response) => {
     response.send("Good day " + request.params.name + ". What can I do for you today?")
+})
+app.get('/tip/:total/:tipPercentage', (request, response) => {
+    const total = parseInt(request.params.total)
+    const tipPercentage = parseInt(request.params.tipPercentage)
+    const operation = request.query.operation
+    if(operation === 'multiply') {
+        response.send(`The tip will be  $${total*(tipPercentage/100)}.`)
+}   else {
+        response.send("Cannot calculate")
+}
 })
 app.listen(PORT, (request, response) =>{
     console.log(`Server is listening from port ${PORT}.`)
