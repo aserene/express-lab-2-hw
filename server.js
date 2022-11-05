56f37dcb3fedf5a9790c5bb3a08c0345143caa6b
Athena Serene <aserenehoosang@gmail.com>
2022-11-05 12:40:39 -0700

    Greeting express application created

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3003
 app.get('/greeting/:name', (request, response) => {
-    response.send("Merhaba " + request.params.name + ". Nasilsin?")
+    response.send("Good day " + request.params.name + ". What can I do for you today?")
 })
 app.listen(PORT, (request, response) =>{
     console.log(`Server is listening from port ${PORT}.`)
