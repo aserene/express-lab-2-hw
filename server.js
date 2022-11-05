1ba41a0202b3e1db3e761721ffcbefa871e5b815
Athena Serene <aserenehoosang@gmail.com>
2022-11-05 14:08:37 -0700

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ app.get('/tip/:total/:tipPercentage', (request, response) => {
         response.send("Cannot calculate")
 }
 })
+app.get('/magic/:phrase', (request, response) => {
+    // spaces in url is indicated as %20
+    const magicAnswer = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    // console.log(magicAnswer)
+
+    const randomAnswer = magicAnswer[Math.floor(Math.random() * (magicAnswer.length))]
+    console.log(randomAnswer)
+    const phrase = request.params.phrase
+    response.send(`${phrase}?` + `<h1>${randomAnswer}</h1>`)
+})
 app.listen(PORT, (request, response) =>{
     console.log(`Server is listening from port ${PORT}.`)
 })
